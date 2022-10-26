1a9f125e273017b0897d33c352b035137c03fe73
patricioreguera <reguerapatricio@gmail.com>
2022-10-27 00:06:57 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+	return (
+		<div className="flex  justify-center">
+			<h1 className="font-bold text-2xl text-blue-900">Curring</h1>
+		</div>
+	);
+}
+
+export default App;
