d8ef91fafbe9905cf509d03626d72faab269937d
Juan M Scaparro <juanscaparro@gmail.com>
2022-11-12 20:47:19 -0300

    feat: Create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import { Navbar } from "./components";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Projects from "./pages/projects/Projects";
+import Project from "./pages/project/Project";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Proyectos</h1>
+			<Navbar/>
+			<Routes>
+				<Route
+					path="/"
+					element={ <Home/> }
+				/>
+				<Route
+					path="/projects"
+					element={ <Projects/> }
+				/>
+				<Route
+					path="/project"
+					element={ <Project/> }
+				/>
+			</Routes>
 		</div>
 	);
 }
---
+++ src/pages/project/Project.tsx
@@ -0,0 +1,9 @@
+import './Project.css';
+
+function Project() {
+  return (
+    <div>Project</div>
+  )
+}
+
+export default Project
